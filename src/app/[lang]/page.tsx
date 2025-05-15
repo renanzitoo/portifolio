2734c3c4f79b5a83fa91e977fa8e1f8bd9863b27
Renan Costa <91814882+renanzitoo@users.noreply.github.com>
2025-05-15 17:17:21 -0300

    teste

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -1,35 +1,44 @@
-import { getMessages } from "@/lib/i18n";
-import { Locale } from "@/lib/i18n";
+
+import { getMessages, Locale } from "@/lib/i18n";
 import About from "../components/About/About";
 import Projects from "../components/Projects/Projects";
 import HighlightedProjects from "../components/High/HighlightedProjects";
 import Contact from "../components/Contact/Contact";
 import { NavBar } from "../components/navBar/NavBar";
+import type { Metadata } from "next";
 
-interface Props {
+// ✅ Tipagem correta do `generateMetadata`
+export async function generateMetadata({
+  params,
+}: {
   params: { lang: Locale };
-}
-
-export async function generateMetadata({ params }: Props) {
+}): Promise<Metadata> {
   const messages = await getMessages(params.lang);
   return {
-    title: "Renan Costa", 
+    title: "Renan Costa",
+    icons: {
+      icon: "/profile.jpeg",
+    },
   };
 }
 
-export default async function Page({ params }: Props) {
+// ✅ Tipagem correta do componente Page
+export default async function Page({
+  params,
+}: {
+  params: { lang: Locale };
+}) {
   const messages = await getMessages(params.lang);
 
   return (
     <>
-    <NavBar lang={params.lang} />
-    <div className="px-4 sm:px-12 lg:px-32 xl:px-12 max-w-4xl mx-auto">
-      
-      <About messages={messages} />
-      <Projects messages={messages} />
-      <HighlightedProjects messages={messages} />
-      <Contact messages={messages} />
-    </div>
+      <NavBar lang={params.lang} />
+      <div className="px-4 sm:px-12 lg:px-32 xl:px-12 max-w-4xl mx-auto">
+        <About messages={messages} />
+        <Projects messages={messages} />
+        <HighlightedProjects messages={messages} />
+        <Contact messages={messages} />
+      </div>
     </>
   );
 }
